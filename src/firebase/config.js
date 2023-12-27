8f4519ce2ec02de0d421705652e5e9d15bc323b8
Warner GG <warnerg72000@gmail.com>
2023-12-26 18:20:15 -0600

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// AGREGA TUS CREDENCIALES AQUI
+const firebaseConfig = {
+  apiKey: "AIzaSyCNQi2QO21geLkCJyHtz6ZU38d5wRNdSAM",
+  authDomain: "restaurantegaitanabud-e4081.firebaseapp.com",
+  projectId: "restaurantegaitanabud-e4081",
+  storageBucket: "restaurantegaitanabud-e4081.appspot.com",
+  messagingSenderId: "826073005407",
+  appId: "1:826073005407:web:54df6078649fc12b174aea",
+};
+
+export default firebaseConfig;
